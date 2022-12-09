a835ad9584782b4c368374718dedf24e267642eb
MubarakYero <yeromubaaraak@gmail.com>
2022-12-09 15:34:43 -0800

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+ii#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Comment
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
