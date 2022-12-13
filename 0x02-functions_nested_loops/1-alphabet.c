b34fe1de44fb12500f0aaa343f5c42bf8f54e6ad
MubarakYero <yeromubaaraak@gmail.com>
2022-12-13 14:50:48 -0800

    Added all files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *
+ * main - Comment
+ *
+ * Return: 0
+ */
+
+
+void print_alphabet(void)
+{
+char atoz = 'a';
+while (atoz <= 'z')
+{
+_putchar(atoz);
+atoz++;
+}
+_putchar('\n');
+}
